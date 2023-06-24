3c6999c225f1241b13d26173962940fc06ba2202
Farahtout <farahhtout15@gmail.com>
2023-06-24 13:52:31 +0300

    requirements.txt+ fileapi

--- src/modules/urls.py
+++ src/modules/urls.py
@@ -4,13 +4,13 @@
 from django.urls import path
 from . import views
 from modules.views import *
-
+from . import api
+from modules.api import *
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('checkemail', views.checkemail, name='checkemail'),
     path('mycalendly/(?P<user>\d+)/$', views.mycalendly, name='mycalendly'),
-    
     path('drboard/(?P<user>\d+)/$', views.drboard, name='drboard'),
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
@@ -21,6 +21,9 @@ urlpatterns = [
     path('codeVerif/(?P<user>\d+)/<hashed_number>', views.codeVerif, name='codeVerif'),
     path('profilep/',views.profilep,name="profilep"),
     path('profiled/',views.profiled,name="profiled"),
+    path('login-api/', LoginViewApi.as_view(), name='login-api'),
+    # path('forget-api/',api.forgetApi,name="forget"),
+    path('register-api/', RegisterViewApi.as_view(), name='register-api'),
 
 
 ]
